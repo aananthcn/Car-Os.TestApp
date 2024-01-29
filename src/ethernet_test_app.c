f636991b1af87781c34af069f612aae7b150974d
Aananth C N <c.n.aananth@gmail.com>
2024-01-29 22:55:23 +0530

    Added some detailed test functions for Ethernet
    This commit contains some application code polluted with ENC28J60
    driver. This will be improved in the future commits.

--- src/ethernet_test_app.c
+++ src/ethernet_test_app.c
@@ -28,10 +28,79 @@
 #include <TcpIp.h>
 
 
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(ethernet_test_app, LOG_LEVEL_DBG);
+
+
+
+// 1 = read_eth_register()
+// 2 = read_mac_mii_register()
+// 3 = read_phy_register()
+// * = TcpIp stack operations
+#define ENC28J60_SPI_TEST 10
+
+
+// test-case:1 enc28j60_write_reg(ERXSTH, HI_BYTE(RX_BUF_BEG));
+void read_eth_register(void) {
+	static u8 rd_ptr_l;
+
+	// After reset the value @ERDPTL must be == 0xFA
+	rd_ptr_l = enc28j60_read_reg(ERDPTL);
+	LOG_DBG("ERDPTL: 0x%02x", rd_ptr_l);
+}
+
+
+// test-case:2 enc28j60_write_reg(MACON1, MACON1_MARXEN | MACON1_TXPAUS | MACON1_RXPAUS);
+// expected value: 0x00
+void read_mac_mii_register(void) {
+	static u8 m_reg;
+
+	// If initialized as above, MACON1 should contain 0b0000_1101 = 0x0D, but 0x00 is the Read value
+	m_reg = enc28j60_read_reg(MACON1);
+	LOG_DBG("MACON1: 0x%02x", m_reg);
+
+	// Max Frame Len. High Byte should be 0x06
+	m_reg = enc28j60_read_reg(MAMXFLH);
+	LOG_DBG("MAMXFLH: 0x%02x", m_reg);
+}
+
+
+// test-case:3 just read PHID1 and PHID2 and print PHY ID and REV.ID of the device. 
+void read_phy_register(void) {
+        static uint16 phid1, phid2;
+	static uint32 phy_id;
+	static uint8 phy_rev;
+
+	phy_id = 0;
+	phy_rev = 0;
+
+        phid1 = enc28j60_read_phy(PHID1);
+	LOG_DBG("PHID1: 0x%04x", phid1);
+
+        phid2 = enc28j60_read_phy(PHID2);
+	LOG_DBG("PHID2: 0x%04x", phid2);
+
+        phy_id = ((phid2 & 0xFC00) >> 10) << 19 /*bits[15:10] --> bits[24:19]*/ |  phid1 << 3 /*bits[18:3]*/;
+        LOG_DBG("PHY ID: 0x%08x", phy_id); // expected value 0x280418
+
+        phy_rev = (uint8) phid2 & 0x0F;
+        LOG_DBG("PHY RevID: 0x%02x", phy_rev);
+}
+
+
 // Called by Os for every 100 ms
 TASK(Ethernet_Tasks) {
-#if ETH_DRIVER_MAX_CHANNEL > 0
+#if ENC28J60_SPI_TEST == 1
+	read_eth_register();
+#elif ENC28J60_SPI_TEST == 2
+	read_mac_mii_register();
+#elif ENC28J60_SPI_TEST == 3
+	read_phy_register();
+#else
+ #if (ETH_DRIVER_MAX_CHANNEL > 0)
 	TcpIp_MainFunction();
 	macphy_periodic_fn();
+ #endif
 #endif
+	k_sleep(K_SECONDS(1));
 }
