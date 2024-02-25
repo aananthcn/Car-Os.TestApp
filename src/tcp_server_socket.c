740b32c8f2b74f893f9f30f405322a9a3cb8ca08
Aananth C N <c.n.aananth@gmail.com>
2024-02-25 14:10:01 +0530

    Added more comments to the tcp_server_socket.c

--- src/tcp_server_socket.c
+++ src/tcp_server_socket.c
@@ -28,13 +28,14 @@
 static u8 TcpState = TCP_CLOSED;
 
 
+// initialize server socket to start a comm. session
 int tcp_socket_init(void) {
-	int retval = -1;
+	int retval = -1; // initialization is not complete
 	uint16 port = 1000; // change this if you want.
 	Std_ReturnType tcp_retstat;
 
 
-
+	// bind socket to ip address and port
 	if (TcpState == TCP_CLOSED) {
 		tcp_retstat = TcpIp_Bind(0, 0, &port);
 		if (tcp_retstat == E_OK) {
@@ -42,6 +43,7 @@ int tcp_socket_init(void) {
 		}
 	}
 
+	// listen to incoming connections; accept will happen within TcpIp module
 	if ((TcpState == TCP_BIND) || (TcpState == TCP_LISTEN)) {
 		tcp_retstat = TcpIp_TcpListen(0, 0); // call once, this will return immediately, but an accept call back will be called.
 		if (tcp_retstat == E_OK)  {
@@ -52,28 +54,36 @@ int tcp_socket_init(void) {
 		}
 	}
 
+	// check if initialization is complete
 	if (TcpState == TCP_ACCEPT) {
-		retval = 0;
+		retval = 0; // initialization is complete
 	}
 
 	return retval;
 }
 
 
+// This function is cyclically called from the Ethernet_Tasks defined in ethernet_test.c
+//
+// This is basically a different form of Echo server, which will first expect some data
+// to be sent by a client, so that it can respond to it.
 void tcp_socket_main(void) {
 	uint8_t eth_data[1500];
 	int data_len;
 	char reply[] = "Car-OS # Hey \"tcp_client\", you sent me: \"";
 	uint8_t reply_len;
 
-	// frame a response
+
+	// frame a response on the same mem buffer
 	strcpy(eth_data, reply);
 	reply_len = strlen(reply);
 
+	// initialize socket if it isn't initialized
 	if (TcpState < TCP_ACCEPT) {
 		tcp_socket_init();
 	}
 
+	// cyclically receive and send on an initialized socket
 	if (TcpState >= TCP_ACCEPT) {
 		data_len = TcpIp_recv(eth_data+reply_len);
 
